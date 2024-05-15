3c56876009ee631dfa0ad5823e9654925c3fac17
Controllerdestiny <523321293@qq.com>
2024-05-15 16:11:01 +0800

    fix: private msg decode

--- MorMorAdapter/Model/Action/Receive/BroadcastArgs.cs
+++ MorMorAdapter/Model/Action/Receive/BroadcastArgs.cs
@@ -4,6 +4,7 @@ namespace MorMorAdapter.Model.Action.Receive;
 
 
 [ProtoContract]
+[ProtoInclude(501, typeof(PrivatMsgArgs))]
 public class BroadcastArgs : BaseAction
 {
     [ProtoMember(5)] public string Text { get; set; }
